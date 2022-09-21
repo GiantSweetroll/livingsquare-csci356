29d954ac9e1912f07a6eeb118ac9910fd92fa8f4
Raymond-King <raymondking02@hotmail.com>
2022-09-21 12:06:01 +1000

    updated lvl1

---
+++ Living Square/Assets/Scripts/Bridge.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+	Animator BridgeAnimator;
+	bool bridgeOpen;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        bridgeOpen = false;
+		BridgeAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O) )
+		{
+			bridgeOpen=true;
+			BridgeAnimator.SetTrigger("Open");
+		}
+    }
+	
+	void reset()
+	{
+		if(bridgeOpen==true){
+				bridgeOpen=false;
+				BridgeAnimator.SetTrigger("Close");
+		}
+	}
+}
---
+++ Living Square/Assets/Scripts/playerReset.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerReset : MonoBehaviour
+{
+	GameObject player;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+	
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			Invoke(nameof(reset), 1.5f);
+		}
+	}
+	
+	void reset()
+	{
+		player.transform.position = new Vector3(0,2,0);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
