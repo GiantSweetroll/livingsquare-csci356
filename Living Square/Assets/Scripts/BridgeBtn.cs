94ad58d8a5101aacbffdcfefe6e92dfe743c6fbb
Raymond-King <raymondking02@hotmail.com>
2022-09-26 15:43:51 +1000

    Scene switch from lvl 1 to 2
    Button at end of level switches scenes, can use this to further change between levels

---
+++ Living Square/Assets/Scripts/BridgeBtn.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeBtn : MonoBehaviour
+{
+	private GameObject button;
+	public GameObject Bridge;
+	public bool bridgeOpen;
+	Animator BridgeAnimator;
+	Animator ButtonAnimator;
+	private AudioSource audiosource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		audiosource = GetComponent<AudioSource>();
+        button = this.gameObject;
+		BridgeAnimator = Bridge.GetComponent<Animator>();
+		ButtonAnimator = this.GetComponent<Animator>();
+    }
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Input.GetMouseButtonDown(0))
+		{
+			if(Physics.Raycast(ray,out hit) && hit.collider.gameObject == gameObject)
+			{
+				ButtonAnimator.SetTrigger("Down");
+				audiosource.Play(0);
+				Invoke(nameof(up), 0.5f);
+				if(bridgeOpen==false)
+				{
+					Invoke(nameof(open), 0f);
+				} else {
+					Invoke(nameof(close), 0f);
+				}
+			}
+		}
+    }
+	
+	void up()
+	{
+		ButtonAnimator.SetTrigger("Up");
+		Invoke(nameof(idle),1f);
+	}
+	
+	void idle()
+	{
+		ButtonAnimator.SetTrigger("Idle");
+	}
+	
+	void open()
+	{
+		BridgeAnimator.SetTrigger("Open");
+		bridgeOpen=true;
+	}
+	
+	void close()
+	{
+		BridgeAnimator.SetTrigger("Close");
+		bridgeOpen=false;
+		Invoke(nameof(reset), 2f);
+	}
+	
+	void reset()
+	{
+		BridgeAnimator.SetTrigger("Idle");
+	}
+}
--- Living Square/Assets/lvl1NextScene.cs
+++ Living Square/Assets/lvl1NextScene.cs
@@ -1,25 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Button : MonoBehaviour
+public class lvl1NextScene : MonoBehaviour
 {
 	private GameObject button;
-	public GameObject Bridge;
-	public bool bridgeOpen;
-	Animator BridgeAnimator;
 	Animator ButtonAnimator;
 	private AudioSource audiosource;
-
+	
     // Start is called before the first frame update
     void Start()
     {
-		audiosource = GetComponent<AudioSource>();
+        audiosource = GetComponent<AudioSource>();
         button = this.gameObject;
-		BridgeAnimator = Bridge.GetComponent<Animator>();
 		ButtonAnimator = this.GetComponent<Animator>();
     }
-	
 
     // Update is called once per frame
     void Update()
@@ -33,16 +29,11 @@ public class Button : MonoBehaviour
 				ButtonAnimator.SetTrigger("Down");
 				audiosource.Play(0);
 				Invoke(nameof(up), 0.5f);
-				if(bridgeOpen==false)
-				{
-					Invoke(nameof(open), 0f);
-				} else {
-					Invoke(nameof(close), 0f);
-				}
+				SceneManager.LoadScene("Scene_lv2");
 			}
 		}
     }
-	
+    
 	void up()
 	{
 		ButtonAnimator.SetTrigger("Up");
@@ -52,23 +43,5 @@ public class Button : MonoBehaviour
 	void idle()
 	{
 		ButtonAnimator.SetTrigger("Idle");
-	}
-	
-	void open()
-	{
-		BridgeAnimator.SetTrigger("Open");
-		bridgeOpen=true;
-	}
-	
-	void close()
-	{
-		BridgeAnimator.SetTrigger("Close");
-		bridgeOpen=false;
-		Invoke(nameof(reset), 2f);
-	}
-	
-	void reset()
-	{
-		BridgeAnimator.SetTrigger("Idle");
 	}
 }
