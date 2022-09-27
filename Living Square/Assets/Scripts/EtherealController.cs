8aee223b8acc67f3b75463eff4c158db0124d2fe
Lyon Fan <lyoncfan@outlook.com>
2022-09-27 13:46:47 +1000

    Some Ethereal Behavior Changes
    Changed:
    - added layers for player , ethereal, passable walls
    - limited ethereal distance from main body
    - ethereal now collides with non passable objects
    - removed sample scene not in use

--- Living Square/Assets/Scripts/EtherealController.cs
+++ Living Square/Assets/Scripts/EtherealController.cs
@@ -9,11 +9,16 @@ public class EtherealController : MonoBehaviour{
 ==============================================================================*/
 	//player settings variables
     public float SPEED = 5f;
+    public float DISTANCE_LIMIT = 10f;
+    public float DISTANCE_LIMIT_SQUARED = 100f;
 
 	//player internal variables
 	private float mvX;
 	private float mvZ;
+	private Vector3 startPos;
 	private Vector3 mvDir;
+	private Vector3 mvPos;
+	private Vector3 mvPosClamped;
 
 	//player Components
 	private Rigidbody thisRBody;
@@ -25,7 +30,13 @@ public class EtherealController : MonoBehaviour{
 	// Start is called before the first frame update ie. Constructor
 	void Start(){
 		//get the components needed
-		thisRBody = GetComponent<Rigidbody>(); 
+		thisRBody = GetComponent<Rigidbody>();
+		//get the starting position
+		startPos = transform.position;
+		//ignore collision with the player itself when ethereal
+		Physics.IgnoreLayerCollision(11, 8);
+		//ignore collision Ethereal layer 11 with objects in passable layer 10
+		Physics.IgnoreLayerCollision(11, 10);
 	}
 
 
@@ -48,7 +59,18 @@ public class EtherealController : MonoBehaviour{
 	void FixedUpdate(){
 		//calculate direction vector
 		mvDir = transform.forward * mvZ + transform.right * mvX;
-		//move body
-		thisRBody.MovePosition(transform.position + mvDir * Time.fixedDeltaTime * SPEED);
+		//calculate moved position
+		mvPos = transform.position + mvDir * Time.fixedDeltaTime * SPEED;
+		//check if greater than distance limit
+		if((mvPos - startPos).sqrMagnitude > DISTANCE_LIMIT_SQUARED){
+			//clamp the magnitude to the distance limit
+			mvPosClamped = Vector3.ClampMagnitude(mvPos - startPos, DISTANCE_LIMIT);
+			//move body to clamped distance from radius center in direction
+			thisRBody.MovePosition(mvPosClamped + startPos);
+		}
+		else{
+			//move body
+			thisRBody.MovePosition(mvPos);
+		}
 	}
 }
